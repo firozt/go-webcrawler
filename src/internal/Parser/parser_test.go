3ee50e32fb8201cc72964d1d2f370c8b266ac89c
firozt <firozt03@gmail.com>
2025-12-04 20:04:34 +0000

    tests/added_dfs

--- src/internal/Parser/parser_test.go
+++ src/internal/Parser/parser_test.go
@@ -2,7 +2,11 @@ package parser
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 	"testing"
+
+	"golang.org/x/net/html"
 )
 
 func TestRelativePathToUrl(t *testing.T) {
@@ -47,6 +51,57 @@ func TestAbsolutePathToUrl(t *testing.T) {
 				t.Errorf("unexpected error: %v", err)
 			} else if actual != expected {
 				t.Errorf("got %v, want %v", actual, expected)
+			}
+		})
+	}
+}
+
+type DFSTestCase struct {
+	html            string
+	expectedLinks   []string
+	expectedContent []string
+}
+
+func TestDFS(t *testing.T) {
+	// inputs
+	testCases := []DFSTestCase{
+		{
+			`<div>Hello <a href="https://example.com">world</a></div>`,
+			[]string{"https://example.com"},
+			[]string{"Hello ", "world"},
+		},
+		{
+			`<div>Start <div>Nested <a href="https://nested.com/page">link</a></div> End</div>`,
+			[]string{"https://nested.com/page"},
+			[]string{"Start ", "Nested ", "link", " End"},
+		},
+		{
+			`<p><a href="https://one.com">One</a> and <a href="https://two.com">Two</a></p>`,
+			[]string{"https://one.com", "https://two.com"},
+			[]string{"One", " and ", "Two"},
+		},
+		{
+			`<span>Just some text without links.</span>`,
+			[]string{},
+			[]string{"Just some text without links."},
+		},
+	}
+	for i, testcase := range testCases {
+		doc, err := html.Parse(strings.NewReader(testcase.html))
+
+		if err != nil {
+			t.Errorf("Unexpected error in test %v", err)
+		}
+
+		link, content := []string{}, []string{}
+		t.Run(fmt.Sprintf("case_%d", i), func(t *testing.T) {
+			dfs(doc, &content, &link)
+
+			if !reflect.DeepEqual(link, testcase.expectedLinks) {
+				t.Errorf("wanted %v got %v", testcase.expectedLinks, link)
+			}
+			if !reflect.DeepEqual(content, testcase.expectedContent) {
+				t.Errorf("wanted %v got %v", testcase.expectedContent, content)
 			}
 		})
 	}
