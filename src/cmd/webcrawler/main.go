5e12cba73256bc04049e57451f25994bb07f392a
firozt <firozt03@gmail.com>
2025-12-04 22:58:24 +0000

    feature/web server component initialised

--- src/cmd/webcrawler/main.go
+++ src/cmd/webcrawler/main.go
@@ -5,62 +5,59 @@ to handle webcrawling given parameters
 
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-
-	parser "github.com/firozt/crawler/src/internal/Parser"
-	// "github.com/firozt/crawler/src/internal/Repository"
-	"github.com/firozt/crawler/src/internal/ThreadSafeQueue"
-)
+import server "github.com/firozt/crawler/src/internal/Server"
 
 func main() {
+	var HOSTNAME string = "localhost"
+	var PORT string = "8080"
 	// crawlSite("https://en.wikipedia.org/wiki/Chair")
 	// db := repository.InitDB() // creates db conn
 	// defer db.Close()
-	crawlSite("https://books.toscrape.com/index.html")
+	// crawlSite("https://books.toscrape.com/index.html")
 	// pageRepo := repository.NewPagesRepository(db) // creates DAO for pages table
+	server.InitWebServer(HOSTNAME, PORT)
 
 }
 
-func crawlSite(url string) {
-	// initial link queue
-	queue := ThreadSafeQueue.NewThreadSafeQueue[string]()
-	body := parser.ParseSite(url)
-	text, links := parser.GetTextAndLinks(body)
-	fmt.Println(len(text))
-	for _, link := range links {
-		queue.Enqueue(link)
-	}
-	fmt.Println("Links:\n", links)
+// }
 
-	// setup worker pool
-	var wg sync.WaitGroup
-	numWorkers := 5
+// func crawlSite(url string) {
+// 	// initial link queue
+// 	queue := ThreadSafeQueue.NewThreadSafeQueue[string]()
+// 	body := parser.ParseSite(url)
+// 	text, links := parser.GetTextAndLinks(body)
+// 	fmt.Println(len(text))
+// 	for _, link := range links {
+// 		queue.Enqueue(link)
+// 	}
+// 	fmt.Println("Links:\n", links)
 
-	worker := func(id int) {
-		defer wg.Done()
-		for j := 0; j < 2; j++ {
-			link, ok := queue.Dequeue()
-			if !ok {
-				// q empty
-				// TODO, wait for links
-				return
-			}
-			fmt.Printf("Worker %d processing %s\n", id, link)
-			body := parser.ParseSite(link)
-			_, newlinks := parser.GetTextAndLinks(body)
-			fmt.Println("Links:\n", newlinks)
-		}
-	}
+// 	// setup worker pool
+// 	var wg sync.WaitGroup
+// 	numWorkers := 5
 
-	// Start workers
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go worker(i)
-		time.Sleep(500000000)
-	}
-	wg.Wait()
+// 	worker := func(id int) {
+// 		defer wg.Done()
+// 		for j := 0; j < 2; j++ {
+// 			link, ok := queue.Dequeue()
+// 			if !ok {
+// 				// q empty
+// 				// TODO, wait for links
+// 				return
+// 			}
+// 			fmt.Printf("Worker %d processing %s\n", id, link)
+// 			body := parser.ParseSite(link)
+// 			_, newlinks := parser.GetTextAndLinks(body)
+// 			fmt.Println("Links:\n", newlinks)
+// 		}
+// 	}
 
-}
+// 	// Start workers
+// 	for i := 0; i < numWorkers; i++ {
+// 		wg.Add(1)
+// 		go worker(i)
+// 		time.Sleep(500000000)
+// 	}
+// 	wg.Wait()
+
+// }
--- src/internal/Server/server.go
+++ src/internal/Server/server.go
@@ -3,3 +3,27 @@ This file handles all endpoint logic, and exposing endpoints for http requests
 */
 
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// initialise the http web server
+func InitWebServer(HOSTNAME string, PORT string) {
+	// initialise new server mux to handle traffic flow
+	mux := http.NewServeMux()
+
+	// ============= ENDPOINT MAPPINGS ============= //
+
+	mux.HandleFunc("/", HandleRoot)
+
+	// ============================================= //
+
+	fmt.Printf("Server listening to %v:%v\n", HOSTNAME, PORT)
+	http.ListenAndServe(fmt.Sprintf("%v:%v", HOSTNAME, PORT), mux)
+}
+
+func HandleRoot(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(resp, "Hello Word")
+}
